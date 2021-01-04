cce301b53029148b2ecb27c64dcb69fa715d2a32
Clément <jour.clement@gmail.com>
2021-01-04 13:53:43 +0100

    Creating repository

---
+++ src/main/java/com/cjour/SafetyAlert/SafetyAlertSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cjour.SafetyAlert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafetyAlertSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafetyAlertSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cjour/SafetyAlert/SafetyAlertSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjour.SafetyAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyAlertSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
